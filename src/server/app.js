f6a5f986883b95e089f71def4fd9c1352545f39d
Jorge Sanchis <josando210182@gmail.com>
2017-02-17 17:44:55 +0100

    Servidor

--- src/server/app.js
+++ src/server/app.js
@@ -53,7 +53,7 @@ console.log('About to crank up node');
 console.log('PORT=' + port);
 console.log('NODE_ENV=' + environment);
 
-
+/*
 switch (environment) {
   case 'build':
     console.log('** BUILD **');
@@ -77,11 +77,48 @@ switch (environment) {
     // Any deep link calls should return index.html
     app.use('/*', express.static('./src/client/index.html'));
     break;
-}
+}*/
 
+switch (environment) {
+  case 'build':
+    console.log('** BUILD **');
+    app.use(express.static('./build/'));
+    // Any invalid calls for templateUrls are under app/* and should return 404
+    app.use('/app/*', function(req, res, next) {
+      four0four.send404(req, res);
+    });
+    // Any deep link calls should return index.html
+    app.use('/*', express.static('./build/index.html'));
+    console.log('WARNING: OPEN BROWSER WITH HTTPS');
+    https.createServer({
+      key: fs.readFileSync('privkey1.pem'),
+      cert: fs.readFileSync('cert1.pem')
+    }, app).listen(port);
+
+    break;
+  default:
+    console.log('** DEV **');
+    app.use(express.static('./src/client/'));
+    app.use(express.static('./'));
+    app.use(express.static('./tmp'));
+    // Any invalid calls for templateUrls are under app/* and should return 404
+    app.use('/app/*', function(req, res, next) {
+      four0four.send404(req, res);
+    });
+    // Any deep link calls should return index.html
+    app.use('/*', express.static('./src/client/index.html'));
+    app.listen(port, function() {
+      console.log('Express server listening on port ' + port);
+      console.log('env = ' + app.get('env') +
+      '\n__dirname = ' + __dirname +
+      '\nprocess.cwd = ' + process.cwd());
+    });
+    break;
+}
+/*
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
   console.log('env = ' + app.get('env') +
     '\n__dirname = ' + __dirname +
     '\nprocess.cwd = ' + process.cwd());
-});
+});*/
