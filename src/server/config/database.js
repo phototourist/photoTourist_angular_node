ebc973e60a97afb596fb1e643e95e785d7ecdc6c
vieslo82 <vieslo82@gmail.com>
2017-02-20 21:17:39 +0100

    Database subida

---
+++ src/server/config/database.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+        
+        host: '127.0.0.1',
+        user: 'jorge',
+        password: '',
+        database: 'photoTouristBD'
+    }
+);
+
+module.exports.connection = connection;
